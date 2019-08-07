cedb53f7067a2e9f4583238faeadba124f65f2ba
field <768449566@qq.com>
2019-08-07 12:40:05 +0800

    update

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MaskComponent } from './mask/mask';
+@NgModule({
+	declarations: [MaskComponent],
+	imports: [],
+	exports: [MaskComponent]
+})
+export class ComponentsModule {}
