c6615ed0bf5e52d4461a92dcc08ea6ca02ee1f18
field <768449566@qq.com>
2019-08-08 18:36:39 +0800

    update

--- src/pages/boke-mood/boke-mood.ts
+++ src/pages/boke-mood/boke-mood.ts
@@ -22,4 +22,13 @@ export class BokeMoodPage {
     console.log('ionViewDidLoad BokeMoodPage');
   }
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete()
+    }, 2000);
+  }
+
 }
